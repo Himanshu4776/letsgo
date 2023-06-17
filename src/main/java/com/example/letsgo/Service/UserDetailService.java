5cb8226f9017def1dd13ccf8d959cce5cf474f9a
harshrajsingh <harshraj13110@gmail.com>
2023-06-17 13:17:08 +0530

    First Commit.

---
+++ src/main/java/com/example/letsgo/Service/UserDetailService.java
@@ -0,0 +1,9 @@
+package com.example.letsgo.Service;
+
+import com.example.letsgo.Dto.RideDetailsRequestDto;
+
+public interface UserDetailService {
+
+    String generateRideNumber(RideDetailsRequestDto rideDetailsRequestDto);
+
+}
